9bdeab185b0f35fcc7b0c2cb93cbdf413ca33975
6ixty9 <72061948+6ixty9@users.noreply.github.com>
2022-08-01 13:27:22 +0100

    landscape mode + emailjs

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,45 @@
 import "./FormStyles.css"
-
-import React from "react"
+import emailjs from "emailjs-com";
+import React, { useRef } from "react";
 
 
 const Form = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      'service_oj4ikbh', 
+      'template_u2yw1n6', 
+      form.current, 
+      'Ew23-Nbt6g-NbxDJ-')
+      .then((result) => {
+          console.log(result.text);
+          alert("Message Sent!")
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
   return (
     <div className="form">
-        <form action="mailto:Francistrix@gmail.com">
+        <form ref={form} onSubmit={sendEmail}>
             <label>Your Name</label>
-            <input type="text" ></input>
+            <input type="text" name="user_name"></input>
             
             <label>Email</label>
-            <input type="email" ></input>
-
-            <label>Subject</label>
-            <input type="text" ></input>
+            <input type="email" name="user_email"></input>
 
             <label>Message</label>
-            <textarea rows="6" 
+            <textarea rows="6" name="message"
             placeholder="Type Your Message Here" />
 
-            <button className="btn" type="submit">Submit</button>
+            <button className="btn" type="submit"
+            value="Send" >Submit</button>
 
         </form>
     </div>
