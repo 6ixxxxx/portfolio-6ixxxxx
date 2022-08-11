4b85e50d52ba30095d08974fa3e3e2bbcbd5af28
6ixty9 <72061948+6ixty9@users.noreply.github.com>
2022-08-11 06:05:07 +0100

    yeahh

--- src/components/ProjectsCardData.js
+++ src/components/ProjectsCardData.js
@@ -1,28 +1,36 @@
-import project01 from "../assets/project01.png"
-import project2 from "../assets/project2.png"
-import project3 from "../assets/project3.png"
+import portfolio from "../assets/portfolio.png"
+import movies from "../assets/movies.png"
+import memeGen from "../assets/memeGen.png"
+import travelJournal from "../assets/travelJournal.png"
 
 const ProjectsCardData = [
     {
-        imgsrc: project01,
+        imgsrc: portfolio,
         title: "My Portfolio Website",
         text: "My own personal front page to the internet, it's only right it comes first",
         view: "/",
         source: "https://github.com/6ixxxxx/portfolio-6ixxxxx"
     },
     {
-        imgsrc: project2,
+        imgsrc: movies,
         title: "Netflix Clone: Movies Website",
         text: "Search for any movie you want",
         view: "https://6ixxxxx.github.io/movie_app_react/",
         source: "https://github.com/6ixxxxx/movie_app_react"
     },
     {
-        imgsrc: project3,
+        imgsrc: memeGen,
         title: "React Meme Generator",
         text: "Create your very own meme",
         view: "https://6ixxxxx.github.io/react-meme-generator/",
         source: "https://github.com/6ixxxxx/react-meme-generator"
+    },
+    {
+        imgsrc: travelJournal,
+        title: "Travel journal",
+        text: "This was the first project i built testing props",
+        view: "https://6ixxxxx.github.io/travel-journal/",
+        source: "https://github.com/6ixxxxx/travel-journal"
     },
 ]
 
